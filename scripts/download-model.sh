8f6b9742f9898a5305005b460e3d7a9a4677c868
Uchenna Ofoma <uchenna19of@gmail.com>
2025-10-26 19:44:05 +0000

    chore: updated download script

--- scripts/download-model.sh
+++ scripts/download-model.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl 'https://media.githubusercontent.com/media/AustinChris1/Agrosense360_FlaskServer/refs/heads/main/best_agrosense_model.h5?download=true' \
-  --output agrosense360/models/model.h5
+uvx gdown '1xkvaONSV_Y5i4uts6qWYxesaf5LAH7Ll'
+mv best_agrosense_model.h5 ./agrosense360/models/model.h5
